12d6f5241a95d131f3e2176dc750e758d4052bfd
1kain <1kain@mail.ru>
2022-10-22 12:51:03 +0300

    Menu and log

---
+++ base.py
@@ -0,0 +1,29 @@
+import os
+
+
+def writeLog(text):
+    try:
+        with open('log.txt', 'a') as f:
+            f.writelines(text + '\n')
+    except Exception as error:
+        print(error)
+
+
+def readLog():
+    with open('log.txt', 'r') as f:
+        print(f.read())
+
+
+def clearLog():
+    open('log.txt', 'w').close()
+
+
+def initLog():
+    open('log.txt', 'x').close()
+
+
+def deleteLog():
+    os.remove('log.txt')
+
+
+writeLog('asd')
---
+++ menu.py
@@ -0,0 +1,37 @@
+import sys
+from calculator import main as calculator
+from base import readLog, clearLog, deleteLog
+
+menuText = [
+    "*" * 15 + "КАЛЬКУЛЯТОР" + "*" * 15,
+    "Введите номер операции для продолжения:",
+    "1. Сделать вычисление на калькуляторе",
+    "2. Для вывода журнала совершенных операций",
+    "3. Для очистки журнала совершенных операций",
+    "4. Для завершения работы",
+    "*" * 43
+]
+
+for mt in menuText:
+    print(mt)
+
+
+def terminate():
+    deleteLog()
+    sys.exit("Bye!!")
+
+
+menuActions = {
+    '1': calculator,
+    '2': readLog,
+    '3': clearLog,
+    '5': terminate
+}
+
+
+while True:
+    action = input("==>")
+    if action not in menuActions:
+        print("--Выбран неверный пункт меню--")
+    else:
+        menuActions[action]()
